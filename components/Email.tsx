485a5ff7ae7b54864688016cb513705b6b81a8ea
Christal Bell <christalvictoriabell@gmail.com>
2025-01-10 22:18:52 -0800

    error message added

--- components/Email.tsx
+++ components/Email.tsx
@@ -1,11 +1,10 @@
 "use client";
-import { Box, Input, Button } from "@mui/material";
+import { Box, Input, Button, Typography } from "@mui/material";
 
 import React, { useState } from "react";
 
 const Email = () => {
   const [isValid, setIsValid] = useState(true);
-  // console.log(email);
 
   const isEmail = (email: string) => {
     return /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$/.test(
@@ -24,18 +23,27 @@ const Email = () => {
 
   return (
     <Box>
-      <Input
-        placeholder="email address"
-        onChange={(e) => validator(e.target.value)}
-        sx={{
-          border: `1px solid  ${isValid ? "navy" : "red"}`,
-          borderRadius: "2rem",
-          height: "3rem",
-          width: "26.5rem",
-          color: "navy",
-          padding: "1.25rem",
-        }}
-      />
+      <>
+        <Input
+          placeholder="email address"
+          onChange={(e) => validator(e.target.value)}
+          sx={{
+            border: `1px solid  ${isValid ? "navy" : "red"}`,
+            borderRadius: "2rem",
+            height: "3rem",
+            width: "26.5rem",
+            color: "navy",
+            padding: "1.25rem",
+          }}
+        />
+        {isValid ? null : (
+          <Typography
+            sx={{ color: "red", fontSize: ".75rem", marginLeft: "1.75rem" }}
+          >
+            Please enter a valid email address.
+          </Typography>
+        )}
+      </>
       <Button
         variant="outlined"
         sx={{
